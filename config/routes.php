0b5b3025e9084f5d62f9ecf93cd1a7353eab84ad
Emilio <hitsone.sccc@gmail.com>
2015-04-16 18:23:08 -0700

    Adds News section

--- config/routes.php
+++ config/routes.php
@@ -49,10 +49,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-/* ORIGINAL ROUTES--------------------------------------------------
+/*
+----------ORIGINAL ROUTES--------------------------------------------------
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-*/
+----------DEFAULT ROUTE-----------------------------------------------------
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
+*/
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
