362b9bb133db272ca8e87220be20f5db20ce1ed5
MatrixNinja <hitsone.sccc@gmail.com>
2015-04-23 18:34:34 -0700

    Adds Bootswatch Themes and Helpers

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -5,16 +5,19 @@ class News extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->model('news_model');
+		$this->config->set_item('banner','News Banner');
 	}//End __construct function
 
 	public function index()
 	{
 		$data['news'] = $this->news_model->get_news();
 		$data['title'] = 'News archive';
+		
+		//$this->config->set_item('banner','News Banner');
 
-		$this->load->view('templates/header', $data);
+		//$this->load->view('templates/header', $data);
 		$this->load->view('news/index', $data);
-		$this->load->view('templates/footer');
+		//$this->load->view('templates/footer');
 	}//End index function
 
 	public function view($slug = NULL)
@@ -28,9 +31,9 @@ class News extends CI_Controller {
 
 		$data['title'] = $data['news_item']['title'];
 
-		$this->load->view('templates/header', $data);
+		//$this->load->view('templates/header', $data);
 		$this->load->view('news/view', $data);
-		$this->load->view('templates/footer');
+		//$this->load->view('templates/footer');
 	}//End views function
 
 	public function create()
---
+++ application/views/news/index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+The code below comes from controllers/News.php:
+$this->load->view('themes/bootswatch/header');
+========================================================
+The dynamic header and footer need to be added to each new page created; index.php, view.php, create.php, etc..
+
+-Dynamic Header Example:
+		$this->load->view($this->config->item('theme') . 'header');
+*/
+$this->load->view($this->config->item('theme') . 'header');
+?>
+
+<h2><?php echo $title ?></h2>
+
+<?php foreach ($news as $news_item): ?>
+
+        <h3><?php echo $news_item['title'] ?></h3>
+        <div class="main">
+                <?php echo $news_item['text'] ?>
+        </div>
+        <p><a href="<?php echo 'news/' . $news_item['slug'] ?>">View article</a></p>
+
+<?php endforeach ?>
+
+<?php
+//$this->load->view('themes/bootswatch/footer'); 
+$this->load->view($this->config->item('theme') . 'footer');
+?>
