f4885b0d8abee0c674705a95bf88963847d88588
Emilio G. <hitsone.sccc@gmail.com>
2015-05-12 16:33:58 -0700

    Misc Maintenance
    Adds changes to custom_config.php
    Moves and Replaces default header and footer, from Pages.php, to a
    dynamic header and footer for each page in '..\views\pages\'

--- application/config/custom_config.php
+++ application/config/custom_config.php
@@ -2,8 +2,8 @@
 //custom_config.php - a place to save global settings
 
 $config['style'] = 'slate.css';
-$config['banner'] = 'Default Banner';
-$config['title'] = 'Default Title';
+$config['banner'] = 'Default Home Banner';
+$config['title'] = 'ITC260 CodeIgniter';
 $config['copyright'] = 'Default Copyright';
 $config['masthead'] = 'Default MastHead';
 $config['theme'] = 'themes/bootswatch/';
--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,1 +1,3 @@
-<h1>This is the About Page</h1>
+<?php $this->load->view($this->config->item('theme') . 'header'); ?>
+	<h1>This is the About Page</h1>
+<?php $this->load->view($this->config->item('theme') . 'footer'); ?>
